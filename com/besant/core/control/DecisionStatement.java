9ee02cc0e4eb5d8de7d1742bcc305fa983748dc9
Arun26Nura <arunkumarmohan1@gmail.com>
2022-12-27 11:08:11 +0530

    Operators

---
+++ com/besant/core/control/DecisionStatement.java
@@ -0,0 +1,21 @@
+package com.besant.core.control;
+
+public class DecisionStatement {
+    public static void main(String[] args) {
+        int numberOne=15;
+        int numbertwo=10;
+
+        // {} - curly braces or blocks
+        // () - Paranthesis.
+        // Parameters or Arguments
+    boolean comparison= numberOne > numbertwo;
+        if(true){
+            System.out.println("Number One is greater");
+        }
+
+
+
+
+
+    }
+}
---
+++ com/besant/core/operators/BitwiseOperator.java
@@ -0,0 +1,21 @@
+package com.besant.core.operators;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        //complement operator
+        int number=-6; //-(n+1)
+        System.out.println(~number);
+        // Bitwise AND
+        System.out.println(6 & 5);
+        // Bitwise OR
+        System.out.println(6 | 5);
+        // Bitwise XOR
+        System.out.println(6 ^ 5);
+        // Left Shift
+        System.out.println(6 << 2);
+        // Signed Right Shift
+        System.out.println(6 >> 2);
+        // UnSigned Right Shift
+        System.out.println(-6 >>> 2);
+    }
+}
