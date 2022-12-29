b84e80afbe2512884b48068f21bb4167b37d8b49
Arun26Nura <arunkumarmohan1@gmail.com>
2022-12-29 23:50:21 +0530

    Operators

--- com/besant/core/operators/SwitchExpressions.java
+++ com/besant/core/operators/SwitchExpressions.java
@@ -6,10 +6,12 @@ public class SwitchExpressions {
         int numberOfDays= switch (month){
             case "Jan","Mar","May","Aug" -> {
                 System.out.println("Case 1");
-                yield 2;
+                yield 31;
             }
             case "Feb" -> 28;
             case "Apr","Jun" -> 30;
+            default -> 2;
+
         };
         System.out.println(numberOfDays);
     }
